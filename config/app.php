ccca5237eff92fc6ae3bf47408c67dbe3e770820
oomusou <oomusou@gmail.com>
2015-11-12 09:58:21 +0800

    註冊Service Provider

--- config/app.php
+++ config/app.php
@@ -136,6 +136,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Oomusou\HelloWorld\HelloWorldServiceProvider::class,
 
         /*
          * Application Service Providers...
